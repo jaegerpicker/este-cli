a3a3cfdc65d0ea8323e5e87995551fa970de085b
Mike Grabowski <grabbou@gmail.com>
2015-05-25 00:30:46 +0200

    Added custom installBlueprint method

--- blueprints/cursor/index.js
+++ blueprints/cursor/index.js
@@ -4,3 +4,15 @@
  */
 
 'use strict';
+
+var Promise = require('bluebird');
+
+module.exports = {
+  installBlueprint: function(options) {
+    return new Promise(function(resolve) {
+      setTimeout(function() {
+        resolve();
+      }, 1000);
+    });
+  }
+};
