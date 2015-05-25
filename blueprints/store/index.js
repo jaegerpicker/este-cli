16d4762dd588a087b390d0b6051a7ea4af768eb3
Mike Grabowski <grabbou@gmail.com>
2015-05-25 21:32:28 +0200

    Update store blueprint as per docs

--- blueprints/store/index.js
+++ blueprints/store/index.js
@@ -9,11 +9,16 @@ var Promise = require('bluebird');
 var Blueprint = require('../../lib/models/blueprint');
 
 module.exports = {
+
   description: 'Generates new store',
+
+  // Create __name__/actions.js and update state/cursor with appropriate elements
   beforeInstall: function(options) {
     return Promise.all([
+      Blueprint.load('action').install(options),
       Blueprint.load('state').install(options),
       Blueprint.load('cursor').install(options)
     ]);
   }
+
 };
