37cdb722424e9dbc25a1bec6e2d45557b5a3fab8
Mike Grabowski <grabbou@gmail.com>
2015-05-27 20:35:07 +0200

    Added _replaceFilePath test case

--- test/blueprint._replaceFilePath.js
+++ test/blueprint._replaceFilePath.js
@@ -11,8 +11,21 @@ var expect = require('chai').expect;
 
 describe('Blueprint#_replaceFilePath', function() {
 
-  it.skip('test', function() {
+  var blueprint = Blueprint.load('page');
+  var path =  '__root__/__server__/file.js.tpl';
+  var tokens = {
+    __root__: 'src/client',
+    __server__: 'pages'
+  };
 
+  it('should replace all tokens', function() {
+    var replacedPath = blueprint._replaceFilePath(path, tokens);
+    expect(replacedPath).to.include('src/client/pages/file.js');
+  });
+
+  it('should remove template extension', function() {
+    var replacedPath = blueprint._replaceFilePath(path, tokens);
+    expect(replacedPath).to.not.include('.tpl');
   });
 
 });
