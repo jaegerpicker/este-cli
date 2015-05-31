31622d7343e6300e0ecc9fc31ecca7a9f67cb240
Mike Grabowski <grabbou@gmail.com>
2015-05-31 16:41:05 +0200

    Added two new models and started rewriting store-action

--- blueprints/store-action/index.js
+++ blueprints/store-action/index.js
@@ -12,6 +12,7 @@ var recast = require('recast');
 var b = recast.types.builders;
 var n = recast.types.namedTypes;
 var camelCase = require('camel-case');
+var File = require('../../lib/models/File');
 var fs = require('fs');
 var _ = require('lodash');
 
@@ -40,157 +41,86 @@ module.exports = {
 
     // Load store file
     var storePath = path.join(options.rootFolder, options.blueprintName, 'store.js');
-    var data = recast.parse(fs.readFileSync(storePath).toString());
+    var file = new File(fs.readFileSync(storePath));
 
     // Find dispatchToken export declaration
     // Check if actions are imported
-    var dispatchToken = null;
-    var actionImport = null;
-
-    recast.visit(data.program.body, {
-
-      visitImportDeclaration: function(imports) {
-        if (imports.get('source').value.value === './actions') {
-          actionImport = imports;
-        }
-        return false;
-      },
-
-      visitExportDeclaration: function(exports) {
-        this.traverse(exports);
-      },
-
-      visitVariableDeclaration: function(variable) {
-        this.traverse(variable);
-      },
-
-      visitVariableDeclarator: function(declarator) {
-        if (declarator.get('id').value.name === 'dispatchToken') {
-          dispatchToken = declarator;
-          this.abort();
-        }
-        return false;
-      }
-
-    });
+    var dispatchToken = file.getVariable('dispatchToken');
+    var actionImport = file.getImportDeclaration('./actions');
 
     if (!dispatchToken) {
       return Promise.reject('No dispatchToken is exported from your app. Please check your store file');
     }
 
-    var specifiers = null;
-
-    // Import actions if not yet done using ImportNamespaceSpecifiers
-    // @TODO check for `action` collision
     if (!actionImport) {
-
-      actionImport = b.importDeclaration(
-        [b.importNamespaceSpecifier(
-          b.identifier('actions')
-        )],
-        b.literal('./actions')
-      );
-
-      data.program.body.unshift(actionImport);
-
-      specifiers = actionImport.specifiers;
-
-    } else {
-      specifiers = actionImport.get('specifiers').value;
+      actionImport = file.addImportDeclaration('actions', './actions');
     }
 
-    // If not import * from actions,
-    // we need to add our action to objects
-    // it will be the first one in the array
-    if (!n.ImportNamespaceSpecifier.check(specifiers[0])) {
-      var hasAlreadyImportedAction = false;
-
-      recast.visit(specifiers, {
-
-        visitImportSpecifier: function(specifier) {
-          this.traverse(specifier);
-        },
-
-        visitIdentifier: function(identifier) {
-          if (identifier.value.name === actionName) {
-            hasAlreadyImportedAction = true;
-            this.abort();
-          }
-          return false;
-        }
-
-      });
-
-      // Add action if not yet imported
-      if (!hasAlreadyImportedAction) {
-        var newSpecifier = b.importSpecifier(
-          b.identifier(actionName)
-        );
-        specifiers.push(newSpecifier);
-      }
-
+    if (!actionImport.importsVariable(actionName)) {
+      actionImport.importVariable(actionName);
     }
 
-    // ActionCase identifier,
-    // defaults to actionName
-    var actionCaseIdentifier = actionName;
-
-    // If imported with * as x, get x and use for actionCaseIdentifier
-    if (n.ImportNamespaceSpecifier.check(specifiers[0])) {
-      actionCaseIdentifier = specifiers[0].id.name + '.' + actionName;
-    }
-
-    // Create action case
-    var actionCase = b.switchCase(
-      b.identifier(actionCaseIdentifier),
-      [
-        b.breakStatement()
-      ]
-    );
-    actionCase.comments = [b.line(' Put your action specific logic here')];
-
-    // Find switch statement
-    var switchStatement = null;
-    var alreadyHasSwitch = false;
-
-    recast.visit(dispatchToken, {
-
-      visitSwitchStatement: function(switchExpr) {
-        switchStatement = switchExpr;
-        this.traverse(switchStatement);
-      },
-
-      visitSwitchCase: function(switchCase) {
-        var test = switchCase.get('test').value;
-        if (n.Identifier.check(test) && test.name === actionName) {
-          alreadyHasSwitch = true;
-          this.abort();
-        }
-        return false;
-      },
-
-      visitMemberExpression: function(memberExpression) {
-        var object = memberExpression.get('object').value.name;
-        var prop = memberExpression.get('property').value.name;
-        if ((object + '.' + prop) === actionCaseIdentifier) {
-          alreadyHasSwitch = true;
-          this.abort();
-        }
-        return false;
-      }
-
-    });
-
-    if (!switchStatement) {
-      return Promise.reject('No switch statement present in your app. Please check your store file');
-    }
-
-    if (!alreadyHasSwitch) {
-      switchStatement.get('cases').unshift(actionCase);
-    }
+    //
+    //// ActionCase identifier,
+    //// defaults to actionName
+    //var actionCaseIdentifier = actionName;
+    //
+    //// If imported with * as x, get x and use for actionCaseIdentifier
+    //if (n.ImportNamespaceSpecifier.check(specifiers[0])) {
+    //  actionCaseIdentifier = specifiers[0].id.name + '.' + actionName;
+    //}
+    //
+    //// Create action case
+    //var actionCase = b.switchCase(
+    //  b.identifier(actionCaseIdentifier),
+    //  [
+    //    b.breakStatement()
+    //  ]
+    //);
+    //actionCase.comments = [b.line(' Put your action specific logic here')];
+    //
+    //// Find switch statement
+    //var switchStatement = null;
+    //var alreadyHasSwitch = false;
+    //
+    //recast.visit(dispatchToken, {
+    //
+    //  visitSwitchStatement: function(switchExpr) {
+    //    switchStatement = switchExpr;
+    //    this.traverse(switchStatement);
+    //  },
+    //
+    //  visitSwitchCase: function(switchCase) {
+    //    var test = switchCase.get('test').value;
+    //    if (n.Identifier.check(test) && test.name === actionName) {
+    //      alreadyHasSwitch = true;
+    //      this.abort();
+    //    }
+    //    return false;
+    //  },
+    //
+    //  visitMemberExpression: function(memberExpression) {
+    //    var object = memberExpression.get('object').value.name;
+    //    var prop = memberExpression.get('property').value.name;
+    //    if ((object + '.' + prop) === actionCaseIdentifier) {
+    //      alreadyHasSwitch = true;
+    //      this.abort();
+    //    }
+    //    return false;
+    //  }
+    //
+    //});
+    //
+    //if (!switchStatement) {
+    //  return Promise.reject('No switch statement present in your app. Please check your store file');
+    //}
+    //
+    //if (!alreadyHasSwitch) {
+    //  switchStatement.get('cases').unshift(actionCase);
+    //}
 
     return new Promise.fromNode(function(callback) {
-      var modifiedElement = recast.print(data).code;
+      var modifiedElement = recast.print(file.ast).code;
       fs.writeFile(storePath, modifiedElement, callback);
     });
 
--- blueprints/translate/index.js
+++ blueprints/translate/index.js
@@ -5,6 +5,9 @@
 
 'use strict';
 
+var recast = require('recast');
+var b = recast.types.builders;
+
 module.exports = {
 
   skipExistingFiles: true,
@@ -12,7 +15,13 @@ module.exports = {
   description: 'Generates new translation',
 
   afterInstall: function(options) {
-    
+    var messagesFile = path.join(options.rootFolder, 'messages.js');
+    var data = recast.parse(fs.readFileSync(messagesFile).toString());
+
+    return new Promise.fromNode(function(callback) {
+      var modifiedElement = recast.print(data).code;
+      fs.writeFile(statePath, modifiedElement, callback);
+    });
   }
 
 };
